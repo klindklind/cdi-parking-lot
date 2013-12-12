c41edff7173edbcea0e457d86bee9fec16bc1a81
U-VADOM\kli <kli@HEL-6067.corp.vaisala.com>
2013-12-12 15:18:46 +0200

    Added instructions how to get bean in Weld versions older than 2.0.0.Alpha3

--- src/main/java/org/klindklind/CarProducer.java
+++ src/main/java/org/klindklind/CarProducer.java
@@ -28,6 +28,15 @@ public class CarProducer {
         return cars;
     }
 
+    /**
+     * <pre>
+     *  CDI.current() works in Weld version 2.0.0.Alpha3 and newer.
+     *  If you're using older Weld version, you should inject org.jboss.weld.manager.BeanManagerImpl and invoke
+     *  
+     *  beanManagerImpl.instance().select(beanClass).get();
+     * 
+     * </pre>
+     */
     private EngineRoom getEngineRoomBean() {
         return CDI.current().select(EngineRoom.class).get();
     }
